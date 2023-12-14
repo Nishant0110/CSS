0f7dcbb7133c2d9d7b20807dbea02dd8bcb2d0c8
Nishant0110 <nishant.lakhani01@gmail.com>
2023-12-14 10:03:06 +0530

    Done

---
+++ Java Script/Practice/First.js
@@ -0,0 +1,15 @@
+// var a=1
+// var a=5
+// console.log(a);
+// let b=30
+// b=50
+// console.log(b);
+// const c=60
+// console.log(c);
+var a=10
+console.log(a);
+{
+    var a=20
+    console.log(a);
+}
+console.log(a);
