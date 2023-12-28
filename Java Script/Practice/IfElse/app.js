c97c56ad89c136b0b0eb7b2f5bca1a2fb81e1c56
Nishant0110 <nishant.lakhani01@gmail.com>
2023-12-28 20:25:59 +0530

    Done

---
+++ Java Script/Practice/IfElse/app.js
@@ -0,0 +1,31 @@
+let buttons = document.querySelectorAll("button");
+let input = document.querySelector("#input");
+let btn1 = document.querySelector("#one");
+btn1.onclick= (() => {
+    document.querySelector("#input").value=btn1.value;
+})
+
+
+
+
+let buttons=document.write
+
+
+
+
+
+
+// let btn2 = document.querySelector("#two");
+// 
+
+
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         if (button === btn1) {
+//             document.querySelector("#input").value = btn1.value;
+//         }
+//         if (button === btn2) {
+//             document.querySelector("#input").value = btn2.value;
+//         }
+//     });
+// });
