045f9d23c327e57941adfe7c9154316d192d11d5
Nishant0110 <nishant.lakhani01@gmail.com>
2024-01-09 11:58:04 +0530

    Done

--- Java Script/Practice/validation/app.js
+++ Java Script/Practice/validation/app.js
@@ -21,31 +21,49 @@ a.forEach(input => {
 })
 
 document.getElementById("e-mail").addEventListener("keyup", function () {
-    console.log(this.value);
-    // let RegEx = /^([a-z])+$/ // only small apha a-z
-    // let RegEx = /^([A-Z])+$/ // only cap apha A-Z
-    // let RegEx = /^([A-Za-z])+$/ // only cap apha A-Z
     let RegEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9_\.\-])+\.)+([a-zA-Z0-9]{2,4})+$/
     console.log(RegEx.test(this.value));
+    const emailInp = document.getElementById("e-mail")
+    const alertmsg1 = document.querySelector(".alertmsg1")
+    if (!RegEx.test(emailInp.value)) {
+        if (!alertmsg1) {
+            let b = document.createElement("div")
+            b.className = "alertmsg1"
+            b.textContent = "Invalid email address"
+            emailInp.parentNode.appendChild(b)
+        }
+    } else {
+        if (alertmsg1) {
+            emailInp.parentNode.removeChild(alertmsg1)
+        }
+    }
 })
+
+
+
+
+
+
+
+
 // document.getElementById("tel").addEventListener("keyup", function () {
 //     console.log(this.value);
 //     let RegEx =/^\d{10}$/;
 //     console.log(RegEx.test(this.value));
 // })
 
-function validatePhoneNumber() {
-    var telInput = document.getElementById('tel');
-    var telError = document.getElementById('telError');
-    var telValue = telInput.value.trim();
+// function validatePhoneNumber() {
+//     var telInput = document.getElementById('tel');
+//     var telError = document.getElementById('telError');
+//     var telValue = telInput.value.trim();
 
-    // Check if the telephone number is blank or not 10 digits
-    if (telValue === '' || telValue.length !== 10) {
-      telError.textContent = 'Please enter a valid 10-digit telephone number.';
-    } else {
-      telError.textContent = ''; // Clear error message if valid
-    }
-  }
+//     // Check if the telephone number is blank or not 10 digits
+//     if (telValue === '' || telValue.length !== 10) {
+//         telError.textContent = 'Please enter a valid 10-digit telephone number.';
+//     } else {
+//         telError.textContent = ''; // Clear error message if valid
+//     }
+// }
 
 
 // a.onblur=function(){
