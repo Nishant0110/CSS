47571333ee3b5cc4cc1c61b9398bcba09c7d4917
Nishant0110 <nishant.lakhani01@gmail.com>
2024-01-09 08:14:02 +0530

    Done

--- Java Script/Practice/validation/app.js
+++ Java Script/Practice/validation/app.js
@@ -20,33 +20,33 @@ a.forEach(input => {
     }
 })
 
+document.getElementById("e-mail").addEventListener("keyup", function () {
+    console.log(this.value);
+    // let RegEx = /^([a-z])+$/ // only small apha a-z
+    // let RegEx = /^([A-Z])+$/ // only cap apha A-Z
+    // let RegEx = /^([A-Za-z])+$/ // only cap apha A-Z
+    let RegEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9_\.\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    console.log(RegEx.test(this.value));
+})
+// document.getElementById("tel").addEventListener("keyup", function () {
+//     console.log(this.value);
+//     let RegEx =/^\d{10}$/;
+//     console.log(RegEx.test(this.value));
+// })
 
+function validatePhoneNumber() {
+    var telInput = document.getElementById('tel');
+    var telError = document.getElementById('telError');
+    var telValue = telInput.value.trim();
 
+    // Check if the telephone number is blank or not 10 digits
+    if (telValue === '' || telValue.length !== 10) {
+      telError.textContent = 'Please enter a valid 10-digit telephone number.';
+    } else {
+      telError.textContent = ''; // Clear error message if valid
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let a=document.getElementById("firstname")
 
 // a.onblur=function(){
 //     alert("Exit")
