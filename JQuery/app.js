e4f0cd967d85b5f0fe4a7f0fee2222e253a0f57a
Nishant0110 <nishant.lakhani01@gmail.com>
2024-01-13 15:51:43 +0530

    done

---
+++ JQuery/app.js
@@ -0,0 +1,8 @@
+//for span1 syntex
+let syn1 = document.getElementById("span1");
+syn1.innerHTML = `&lt;script src="jquery-3.6.0.min.js">&lt;/script>`;
+
+//for span2 syntex
+let syn2 = document.getElementById("span2");
+syn2.innerHTML = `&lt;script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">&lt;/script>`;
+
