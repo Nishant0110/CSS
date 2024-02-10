64c04ec0b2b220ea1748a4596deb43196d421b5a
Nishant0110 <nishant.lakhani01@gmail.com>
2024-02-10 21:59:50 +0530

    done

---
+++ React-JS/Project1/createmodule/calculator.js
@@ -0,0 +1,8 @@
+export let sumData=(a,b)=>{
+    return a+b;
+}
+export let subData=(a,b)=>{
+    return a-b;
+}
+export let var1="hello"
+// export {sumData,subData,var1};
---
+++ React-JS/Project1/createmodule/index.js
@@ -0,0 +1,5 @@
+import {sumData as nickname,subData,var1} from "./calculator.js";
+
+console.log(nickname(30,20));
+console.log(subData(30,20));
+console.log(var1);
