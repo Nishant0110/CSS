0560a695275bcf83d54ec8323d1b239ec72bf359
Nishant0110 <142566382+Nishant0110@users.noreply.github.com>
2024-03-27 10:53:12 +0530

    Assignment List and Hooks Done

---
+++ React-JS/Assignment/Assignment List & Hooks/app.js
@@ -0,0 +1,18 @@
+let col20 = document.querySelector(".col-20");
+let aElements = col20.querySelectorAll("a");
+
+aElements.forEach(a => {
+    a.onclick = () => {
+        let index = a.classList[0].charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+        let targetQuestion = document.querySelector(`.que-${index}`);
+
+        // Hide all questions
+        document.querySelectorAll('.dis').forEach(question => {
+            question.style.display = 'none';
+        });
+        // Display the selected question
+        targetQuestion.style.display = "block";
+    }
+});
+
+
