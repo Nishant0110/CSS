8521b19c15afb98ab63d4e814223f93a6b3b15f3
nishant <nishant.lakhani01@gmail.com>
2024-04-03 21:04:01 +0530

    Portfolio Navbar created

--- React-JS/e-CommerceOrganic/src/App.jsx
+++ React-JS/e-CommerceOrganic/src/App.jsx
@@ -1,12 +1,9 @@
-
 import './App.css'
-import Navbar from './components/Navbar'
-
+import Navbar from './components/Navbar/Navbar'
 function App() {
-
   return (
     <>
-    <Navbar/>
+      <Navbar/>
     </>
   )
 }
