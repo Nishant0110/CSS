b23d7ab3d82b76c370a400883abbd26406cc9f5f
nishant <nishant.lakhani01@gmail.com>
2024-04-05 22:19:00 +0530

    d

--- Java Script/Practice/First.js
+++ Java Script/Practice/First.js
@@ -6,10 +6,29 @@
 // console.log(b);
 // const c=60
 // console.log(c);
-var a=10
+var a = 10
 console.log(a);
 {
-    var a=20
+    var a = 20
     console.log(a);
 }
 console.log(a);
+document.write();
+var h = 10;
+document.write(h, "<br>");
+var i = "nishant\"bhai\"patel"; BigInt("123456789012345678901234567890");
+document.write(i);
+
+var j = BigInt("123456789012345678901234567890");
+document.write(j);
+
+let num1 = 100;
+let num2 = 50;
+let num3 = 40;
+let sum = num1 + num2;
+num3 -= sum;
+
+
+// num -=5;
+
+document.write("<br>", num3) 
